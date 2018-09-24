8ddcb4c6aa139d41204b001f51f11d107c6d5903
dylan-sk8 <dylan-sk8@hotmail.com>
2018-09-24 15:48:47 +0200

    modify java log to log4j

--- src/main/java/ch/hearc/ig/odi/stringcalculatorlog/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/stringcalculatorlog/StringCalculator.java
@@ -1,21 +1,44 @@
 package ch.hearc.ig.odi.stringcalculatorlog;
 
 
-import java.text.MessageFormat;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class StringCalculator {
 
-    private static Logger LOGGER = Logger.getLogger(StringCalculator.class.getName());
+    private static Logger LOGGER = LogManager.getLogger(StringCalculator.class.getName());
 
-    public static int add(final String numbers) {
+    // Question 1.
+    public StringCalculator() {
+    }
+
+    // Question 2.
+//    public StringCalculator() {
+//
+//    }
+
+    //Question 3.
+//    public StringCalculator() {
+//        try {
+//            FileHandler fl = new FileHandler("log.log", true);
+//            fl.setFormatter(new SimpleFormatter());
+//            LOGGER.addHandler(fl);
+//        } catch (IOException e) {
+//            throw new IllegalStateException("Could not add FileHandler", e);
+//        }
+//    }
+
+
+
+    public int add(final String numbers) {
         String delimiter = ",|\n";
         String numbersWithoutDelimiter = numbers;
         if (numbers.startsWith("//")) {
-            LOGGER.log(Level.INFO, "Delimiter detected");
             int delimiterIndex = numbers.indexOf("//") + 2;
             delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
             LOGGER.info(String.format("Delimiter is '%s'", delimiter));
@@ -24,7 +47,7 @@ public class StringCalculator {
         return add(numbersWithoutDelimiter, delimiter);
     }
 
-    private static int add(final String numbers, final String delimiter) {
+    private int add(final String numbers, final String delimiter) {
         int returnValue = 0;
         List<Integer> negativeNumbers = new ArrayList();
         String[] numbersArray = numbers.split(delimiter);
@@ -33,15 +56,17 @@ public class StringCalculator {
                 Integer numberInt = Integer.parseInt(number);
                 if (numberInt < 0){
                     negativeNumbers.add(numberInt);
-                    LOGGER.warning("Negative number detected: " + numberInt);
+                    LOGGER.warn("Negative number detected: " + numberInt);
                 }
                 else if (numberInt <= 1000)
                     returnValue += Integer.parseInt(number.trim());
+                else
+                    LOGGER.warn(String.format("Number greater than 1000 detected [%s], it'll not be taken into account.", numberInt));
             }
         }
 
         if (negativeNumbers.size() > 0) {
-            LOGGER.log(Level.SEVERE, "Negative not allowed: " + negativeNumbers.toString());
+            LOGGER.fatal("Negative not allowed: " + negativeNumbers.toString());
             throw new RuntimeException("Negative not allowed: " + negativeNumbers.toString());
         }
 
--- src/test/java/ch/hearc/ig/odi/stringcalculatorlog/StringCalculatorTest.java
+++ src/test/java/ch/hearc/ig/odi/stringcalculatorlog/StringCalculatorTest.java
@@ -1,81 +1,82 @@
 package ch.hearc.ig.odi.stringcalculatorlog;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.logging.Logger;
-
 public class StringCalculatorTest {
+
+    private StringCalculator sc;
 
     //Requirement 1
 
     //this test is deleted at requirement 4
 //    @Test(expected = RuntimeException.class)
 //    public void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-//        StringCalculator.add("1,2,3");
+//        sc.add("1,2,3");
 //    }
 
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() {
+        this.sc = new StringCalculator();
     }
 
     @Test
     public void when2NumbersAreUsedThenNoExceptionIsThrown() {
-        StringCalculator.add("1,2");
+        sc.add("1,2");
         Assert.assertTrue("When 2 numbers are used, there is no exception", true);
     }
     @Test(expected = RuntimeException.class)
     public void whenNonNumberIsUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,X");
+        sc.add("1,X");
     }
 
     //Requirement 2
     @Test
     public void whenStringIsEmptyThenZero() {
-        Assert.assertEquals("When the string is empty then zero is returned", 0, StringCalculator.add(""));
+        Assert.assertEquals("When the string is empty then zero is returned", 0, sc.add(""));
     }
 
     // Requirement 3
     @Test
     public void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
-        Assert.assertEquals("When a single number is used, the value returned is the number himself",4, StringCalculator.add("4"));
+        Assert.assertEquals("When a single number is used, the value returned is the number himself",4, sc.add("4"));
     }
 
     @Test
     public void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
-        Assert.assertEquals("When two number are used, the value returned is their sum", 7, StringCalculator.add("3,4"));
+        Assert.assertEquals("When two number are used, the value returned is their sum", 7, sc.add("3,4"));
     }
 
     // Requirement 4
     @Test
     public void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
-        Assert.assertEquals("When multiple numbers are used, the value returned is their sum", 3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+        Assert.assertEquals("When multiple numbers are used, the value returned is their sum", 3+6+15+18+46+33, sc.add("3,6,15,18,46,33"));
     }
 
     // Requirement 5
     @Test
     public void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
-        Assert.assertEquals("When a new line is used between numbers, the value returned is their sum", 3+7+8, StringCalculator.add("3,7\n8"));
+        Assert.assertEquals("When a new line is used between numbers, the value returned is their sum", 3+7+8, sc.add("3,7\n8"));
     }
 
     // Requirement 6
     @Test
     public void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
         Assert.assertEquals("When a delimiter is specified, it is used to separate the numbers," +
-                "the value returned is their sum",9+5+2, StringCalculator.add("//;\n9;5;2"));
+                "the value returned is their sum",9+5+2, sc.add("//;\n9;5;2"));
     }
 
     // Requirement 7
     @Test(expected = RuntimeException.class)
     public void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
-        StringCalculator.add("9,3,-2,3");
+        sc.add("9,3,-2,3");
     }
     @Test
     public void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown() {
         RuntimeException exception = null;
         try {
-            StringCalculator.add("3,4,-2,3,-5,2,-1");
+            sc.add("3,4,-2,3,-5,2,-1");
         } catch (RuntimeException e) {
             exception = e;
         }
@@ -87,6 +88,6 @@ public class StringCalculatorTest {
     @Test
     public void whenOneOrMoreNumbersAreGreaterThan1000IsUsedThenItIsNotIncludedInSum() {
         Assert.assertEquals("When one or more number are greater than 1000, they are not included" +
-                "in the sum", 1003, StringCalculator.add("1000,1,1001,2,1203"));
+                "in the sum", 1003, sc.add("1000,1,1001,2,1203"));
     }
 }
